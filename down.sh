9bc8ba3dcbaca52d8ffe36456e473d8a06e55726
Dynamo Stack <9chakri+ds@gmail.com>
2016-06-01 10:55:31 -0700

    first commit

---
+++ down.sh
@@ -0,0 +1,20 @@
+cd master1
+vagrant halt
+cd ..
+
+cd master2
+vagrant halt
+cd ..
+
+cd master3
+vagrant halt
+cd ..
+
+cd node1
+vagrant halt
+cd ..
+
+cd node2
+vagrant halt
+cd ..
+ 
---
+++ up.sh
@@ -0,0 +1,20 @@
+cd master1
+vagrant up
+cd ..
+
+cd master2
+vagrant up
+cd ..
+
+cd master3
+vagrant up
+cd ..
+
+cd node1
+vagrant up
+cd ..
+
+cd node2
+vagrant up
+cd ..
+ 
