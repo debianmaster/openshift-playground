c94ccce78b391fd70988c3483b87a584b4629d8b
Dynamo Stack <9chakri+ds@gmail.com>
2016-06-21 01:08:22 -0600

    rhel to centos

--- downloadVBox.sh
+++ downloadVBox.sh
@@ -1,7 +1,7 @@
 #https://access.redhat.com/downloads/content/293/ver=1/rhel---7/1.0.1/x86_64/product-software
-echo "Please download rhel7 virtualbox image and place in ~/Downloads"
-echo "RHEL 7.1 Vagrant box for VirtualBox"
-echo "https://access.redhat.com/downloads/content/293/ver=1/rhel---7/1.0.1/x86_64/product-software"
-vagrant box add rhel7  ~/Downloads/rhel-server-virtualbox-7.1-3.x86_64.box
+#echo "Please download rhel7 virtualbox image and place in ~/Downloads"
+#echo "RHEL 7.1 Vagrant box for VirtualBox"
+#echo "https://access.redhat.com/downloads/content/293/ver=1/rhel---7/1.0.1/x86_64/product-software"
+#vagrant box add rhel7  ~/Downloads/rhel-server-virtualbox-7.1-3.x86_64.box
 
 
